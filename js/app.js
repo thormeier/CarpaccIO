2a2cdcadea4cd00fb72f4373e69c943891f98e78
Pascal Thormeier <pascal.thormeier@liip.ch>
2025-01-16 15:43:32 +0100

    Add discount rates.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+import discountRates from '../data/discountRates.json' with { type: "json" }
 import stateTaxData from '../data/stateTaxes.json' with { type: "json" }
 
-console.log(stateTaxData)
+document.querySelector('#state').innerHTML = stateTaxData.map(pair => `<option value="${pair.taxRate}">${pair.state}</option>`)
+
+console.log(discountRates)
