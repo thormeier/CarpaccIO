edf5e749da89c7b6808fbcc2769f85ece1fe5f22
Pascal Thormeier <pascal.thormeier@liip.ch>
2025-01-16 15:48:54 +0100

    Do calcualtions w/o discount.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
 import discountRates from '../data/discountRates.json' with { type: "json" }
 import stateTaxData from '../data/stateTaxes.json' with { type: "json" }
 
-document.querySelector('#state').innerHTML = stateTaxData.map(pair => `<option value="${pair.taxRate}">${pair.state}</option>`)
+const state = document.querySelector('#state')
+const items = document.querySelector('#items')
+const price = document.querySelector('#price')
+
+state.innerHTML = stateTaxData.map(pair => `<option value="${pair.taxRate}">${pair.state}</option>`)
 
 console.log(discountRates)
+
+document.querySelector('#form').addEventListener('submit', e => {
+    e.preventDefault()
+    console.log(items.value * price.value * (1 + state.value))
+    document.querySelector('#result').innerHTML = items.value * price.value * (1 + parseFloat(state.value))
+})
